7275ec0a7349d0cf1631493d0007b5894b13bed8
Лёха Лёха <alfom2001@gmail.com>
2024-06-02 15:35:49 +0300

    Разрешение конфликтов и обновление локального репозитория

--- test1.py
+++ test1.py
@@ -1,19 +1,21 @@
 import platform
-import os
 import psutil
 
 def get_system_info():
+
     system = platform.system()
     architecture = platform.architecture()[0]
     processor = platform.processor()
     ram = psutil.virtual_memory().total / (1024.0 ** 2)
-    
+
+
     return {
         "Операционная система": system,
         "Архитектура": architecture,
         "Процессор": processor,
         "Оперативная память (GB)": round(ram, 2)
     }
+
 
 system_info = get_system_info()
 for key, value in system_info.items():
